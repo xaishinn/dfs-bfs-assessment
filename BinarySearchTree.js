82f6c69a3a4fe57a6d69971b5ac0f538f7a26d26
Brooks Patton <brooks.patton@galvanize.com>
2018-01-09 14:55:08 -0700

    set up for master branch

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -35,26 +35,11 @@ class BinarySearchTree {
 	dfs(cb, node=this.root) {
     // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
-		if(!node) return false
-		cb(node.data)
-		this.dfs(cb, node.left)
-		this.dfs(cb, node.right)
 	}
 
 	bfs(cb, node=this.root) {
     // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
     // Expected order: 25, 15, 197, 17, 97, 9997
-		const nodes = new Queue()
-
-		nodes.enqueue(node)
-
-		while(!nodes.isEmpty) {
-			node = nodes.dequeue()
-			cb(node.data)
-
-			if(node.left) nodes.enqueue(node.left)
-			if(node.right) nodes.enqueue(node.right)
-		}
 	}
 }
 
