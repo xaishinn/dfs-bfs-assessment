f947ca77cbec89a6f23b94b30fa29a7d90094746
Leo McClung <murraythemoose@gmail.com>
2018-02-06 09:51:52 -0700

    finished correct algorithms for dfs and bfs

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -35,14 +35,26 @@ class BinarySearchTree {
 	dfs(cb, node=this.root) {
     // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
 		// Expected order: '25, 15, 17, 197, 97, 9997'
+		if(!node) return;
+		cb(node.data)
+		this.dfs(cb, node.left)
+		this.dfs(cb, node.right)
 	}
 
 	bfs(cb, node=this.root) {
     // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
 	// Expected order: 25, 15, 197, 17, 97, 9997
-		if(!node)return;
-		cb(node.data)
-		this
+		var treeNodes = new Queue()
+		treeNodes.enqueue(node)
+
+		while(treeNodes.isEmpty == false){
+			node = treeNodes.dequeue()
+			cb(node.data)
+
+			if(node.left)treeNodes.enqueue(node.left)
+			
+			if(node.right)treeNodes.enqueue(node.right)
+		}
 	}
 }
 
